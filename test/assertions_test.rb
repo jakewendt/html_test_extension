2fce2d33f55ab1be8aad6b5fd0d234d1f3d00ea8
Jake <github@jake.otherinbox.com>
2010-03-12 13:53:17 -0800

    Added Assertions test

--- test/assertions_test.rb
+++ test/assertions_test.rb
@@ -2,8 +2,12 @@ require File.dirname(__FILE__) + '/test_helper'
 
 class AssertionsTest < ActiveSupport::TestCase
 
-	test "flunking this" do
-#		flunk 
+	test "ValidateFilter contains assert_validates method chain" do
+		methods = Html::Test::ValidateFilter.new(
+			ActionController::Base.new).methods
+		assert methods.include?('assert_validates')
+		assert methods.include?('assert_validates_with_verbosity_control')
+		assert methods.include?('assert_validates_without_verbosity_control')
 	end
 
 end
