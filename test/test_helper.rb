3c2b2bc89599dfc8a04bcb6839462fa93ec88bcc
Jake <github@jake.otherinbox.com>
2010-03-12 12:03:50 -0800

    Added some notes to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require 'rails_generator'
 require 'active_record'
 require 'action_controller'
 #require 'action_controller/test_case'
-require 'active_support/test_case'
+#require 'active_support/test_case'
 
 #	The html_test plugin must be right next to this
 #	OR already have its lib accessible.
@@ -35,8 +35,15 @@ class ActiveSupport::TestCase
 end
 
 
-
 #
 #	I don't know what I did or did not do, but rake test 
 #	failures are UGLY and very unhelpful.
+#	I blame html_test cause my code is very similar
+#	to other plugins and they fail tests nicely.
+#
+#	validating does NOT add to the number of assertions
+#	UNLESS it fails, then the validator asserts an error
+#	so that it shows up.  So don't count the assertions
+#	to see if there is a difference with and without
+#	revalidate_all, because there won't be
 #
